d425e37c7cc0b2ada654e5e4253492362e8805db
Phiko <phgu03@gmail.com>
2023-01-11 12:23:57 +0100

    Two 7s next to each other

--- Dag3/Program.cs
+++ Dag3/Program.cs
@@ -11,28 +11,28 @@ namespace Dag3
         static void Main(string[] args)
         {
             //Console.WriteLine(theBiggestNumber(new int[] { 12, 23, 193, 1312 }));
-            Console.WriteLine(Two7sNextToEachOther(new int[] { 7, 7, 7, 0, 2, 6, 4, 8, 6, 5, 2, 7, 7 }));
+            Console.WriteLine(two7sNextToEachOther(new int[] { 7, 7, 7, 0, 2, 6, 4, 8, 6, 5, 2, 7, 7 }));
             Console.ReadLine();
         }
 
-        //The biggest number
-        private static int theBiggestNumber(int[] number)
-        }
-            return number.Max();
-        }
-
         //Two 7s next to each other
-        private static int Two7sNextToEachOther(int[] numberOfTimes)
+        static int two7sNextToEachOther(int[] numberOfTimes)
         {
-            const int int2 = 0;
-            for (int i = 0; i < numberOfTimes.Length - 1; i++ )
+            int int2 = 0;
+            for (int i = 0; i < numberOfTimes.Length - 1; i++)
             {
-                if (numberOfTimes[i] == 7 && numberOfTimes[i +1] == 7)
+                if (numberOfTimes[i] == 7 && numberOfTimes[i + 1] == 7)
                 {
                     int2++;
                 }
             }
             return int2;
+        }
+
+        //The biggest number
+        static int theBiggestNumber(int[] number)
+        {
+            return number.Max();
         }
     }
 }
