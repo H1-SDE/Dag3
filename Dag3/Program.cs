92648130cdd8a598c2f5bce7d680683e3b095503
Phiko <phgu03@gmail.com>
2023-01-11 14:08:41 +0100

    All are done

--- Dag3/Program.cs
+++ Dag3/Program.cs
@@ -11,7 +11,8 @@ namespace Dag3
         static void Main(string[] args)
         {
             //Console.WriteLine(theBiggestNumber(new int[] { 12, 23, 193, 1312 }));
-            Console.WriteLine(two7sNextToEachOther(new int[] { 7, 7, 7, 0, 2, 6, 4, 8, 6, 5, 2, 7, 7 }));
+            //Console.WriteLine(threeIncreasingAdjacent(new int[] { 1, 4, 1, 1, 4, 3, 4, 5, 6, 7, 5, 4, 9 }));
+            Console.WriteLine(extractString("##abc##def"));  // 123456789
             Console.ReadLine();
         }
 
@@ -33,6 +34,30 @@ namespace Dag3
         static int theBiggestNumber(int[] number)
         {
             return number.Max();
+        }
+
+        //Three increasing adjacent
+        static bool threeIncreasingAdjacent(int[] numbers)
+        {
+            for (int i = 0; i <= numbers.Length - 3; i++)
+            {
+                if (numbers[i] == numbers[i + 1] - 1 && numbers[i] == numbers[i + 2] - 2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static string extractString(string word)
+        {
+            int firstWord = word.IndexOf("##") + 2;
+            int secondWord = word.IndexOf("##", firstWord);
+            if (secondWord - firstWord == 0 || secondWord == -1)
+            {
+                return "empty string";
+            }
+            return word.Substring(firstWord, secondWord - 2);
         }
     }
 }
