a3546a9c000867b0dfce0695d2fc82c45f599ec4
Mikkel <mikkelsorensen1233@gmail.com>
2023-01-11 11:52:49 +0100

    Klaret ved hjælp af 2 for loops

--- Dag3/Program.cs
+++ Dag3/Program.cs
@@ -10,6 +10,24 @@ namespace Dag3
     {
         static void Main(string[] args)
         {
+            for(int j = 1; j <=10 ; j = j + 1) {
+            
+            
+            String text = "";
+
+            for (int i = 1; i <= 10; i = i + 1)
+            {
+                text = text + i * j + " ";
+
+
+
+
+
+            } 
+            Console.WriteLine(text);
+
+            }
         }
+        
     }
 }
