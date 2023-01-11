e9aaf301b6218d2889f46f9adf2b415f5c1cb767
Nima <nimabadiei@gmail.com>
2023-01-11 11:49:33 +0100

    Sieve of Eratosthenes

--- Dag3/Program.cs
+++ Dag3/Program.cs
@@ -10,6 +10,24 @@ namespace Dag3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(String.Join(", ", SieveOfEratosthenes(100)));
+            Console.Read();
+        }
+
+        public static int[] SieveOfEratosthenes(int a)
+        {
+            int[] res = { };
+            var tempRes = res.ToList();
+
+            for (int i = 0; i < a; i++)
+            {
+                if (!(i % 2 == 0 || i % 3 == 0 || i == 1 ) || i == 2 || i == 3) {
+                    tempRes.Add(i);
+                }
+            }
+
+            res = tempRes.ToArray();
+        return res;
         }
     }
 }
