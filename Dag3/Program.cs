a8b5badeeead897a6298ce49ecb08b584db3b89c
Mikkel <mikkelsorensen1233@gmail.com>
2023-01-12 08:48:55 +0100

    k

--- Dag3/Program.cs
+++ Dag3/Program.cs
@@ -10,14 +10,44 @@ namespace Dag3
     {
         static void Main(string[] args)
         {
+            
+            Console.ReadLine();
+        }
+
+        static string Stars(string stars)
+        {
             string stars = "         *";
-            for (int i = 0; i <10; i++)
+            for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(stars);
                 stars = stars.Remove(0, 1);
                 stars += "**";
+                return stars;
             }
-            Console.ReadLine();
+        }
+
+        static string multiTabel (string test)
+        {
+            for (int j = 1; j <= 10; j = j + 1)
+            {
+
+
+                String text = "";
+
+                for (int i = 1; i <= 10; i = i + 1)
+                {
+                    text = text + i * j + " ";
+
+
+
+
+
+                }
+                Console.WriteLine(text);
+            }
+            return test;
         }
     }
 }
+  
+    
