baedbaaf3a77d719cb49a9cb30bfa2d4712a6f2f
Phiko <phgu03@gmail.com>
2023-01-12 08:37:23 +0100

    Update Program.cs

--- Dag3/Program.cs
+++ Dag3/Program.cs
@@ -15,7 +15,83 @@ namespace Dag3
             Console.WriteLine(extractString("##abc##def"));  // 123456789
             Console.ReadLine();
         }
+        
+        public static int[] SieveOfEratosthenes(int a)
+        {
+            int[] res = { };
+            var tempRes = res.ToList();
 
+            for (int i = 0; i < a; i++)
+            {
+                if (!(i % 2 == 0 || i % 3 == 0 || i == 1 ) || i == 2 || i == 3) {
+                    tempRes.Add(i);
+                }
+            }
+
+            res = tempRes.ToArray();
+        return res;
+
+        }
+
+        public static double ToThePowerOf(int a, int b)
+        {
+            double c = a;
+
+            for (int i = 1; i < b; i++)
+            {
+                c *= Convert.ToDouble(a);
+            }
+
+            return c;
+        }
+
+        public static string SumAndAverage(int a, int b)
+        {
+            if (!(a <= b)) { (a, b) = (b, a); }
+
+            int[] arr = { };
+            var arr2 = arr.ToList();
+
+            for (int i = a; i < b+1; i++)
+            {
+                arr2.Add(i);
+            }
+
+            int sum = arr2.Sum();
+            double avg = arr2.Average();
+
+            return "Sum: " + Convert.ToString(sum) + ", Average: " + Convert.ToString(avg);
+        }
+
+        public static string FullSequenceOfLetters(char a, char b)
+        {
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            
+
+            string[] res = { };
+            var res2 = res.ToList();
+
+            int add = 0;
+
+            foreach (char e in alphabet)
+            {
+                if (e == a || e == b) {
+                    add += 1; 
+                    if (add == 2) {
+                        res2.Add(Convert.ToString(e)); 
+                    }
+                }
+                if (add == 1) { res2.Add(Convert.ToString(e)); }
+            }
+
+            return String.Join(" ", res2);
+            //Console.WriteLine(theBiggestNumber(new int[] { 12, 23, 193, 1312 }));
+            //Console.WriteLine(threeIncreasingAdjacent(new int[] { 1, 4, 1, 1, 4, 3, 4, 5, 6, 7, 5, 4, 9 }));
+            Console.WriteLine(extractString("##abc##def"));  // 123456789
+            Console.ReadLine();
+        }
+        
         //Two 7s next to each other
         static int two7sNextToEachOther(int[] numberOfTimes)
         {
